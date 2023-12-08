3a9f421780eea6143c18b9f134ed176a4623e2e2
javiburn <jsarabia98@gmail.com>
2023-12-08 14:14:21 +0100

    CPP05 created

---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/08 14:05:31 by jsarabia          #+#    #+#             */
+/*   Updated: 2023/12/08 14:10:43 by jsarabia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class Bureucrat{
+	private:
+		const std::string	name;
+		int					grade;
+	public:
+};
+
+#endif
