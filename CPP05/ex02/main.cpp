311b3a08fab3be3bb8b58efe01a56fdd593c0eac
javiburn <jsarabia98@gmail.com>
2023-12-13 17:55:45 +0100

    president pardon is functional and works as intended

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -6,19 +6,19 @@
 /*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 14:04:00 by jsarabia          #+#    #+#             */
-/*   Updated: 2023/12/13 17:38:13 by jsarabia         ###   ########.fr       */
+/*   Updated: 2023/12/13 17:54:31 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
-#include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main(void)
 {
 	Shrubbery pepe("home");
 	Robotomy hey("hey");
+	President han("Han Solo");
 	Bureaucrat jose = Bureaucrat("José", 1);
 	std::cout << jose << std::endl;
 	pepe.beSigned(jose);
@@ -26,6 +26,9 @@ int main(void)
 	hey.beSigned(jose);
 	jose.signForm(hey);
 	hey.execute(jose);
+	han.beSigned(jose);
+	jose.signForm(han);
+	han.execute(jose);
 	std::cout << pepe << std::endl;
 	return 0;
 }
