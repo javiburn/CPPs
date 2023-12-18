7c3a72f9acafb05aa820e0d7c190afdb72f0bc52
javiburn <jsarabia98@gmail.com>
2023-12-18 15:26:54 +0100

    parsing invalid inputs like mixing numbers and letters

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 18:26:46 by jsarabia          #+#    #+#             */
-/*   Updated: 2023/12/18 13:30:30 by jsarabia         ###   ########.fr       */
+/*   Updated: 2023/12/18 15:25:56 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,15 +64,17 @@ void	floating(std::string str)
 		std::cout << "char: " << "Non displayable"  << std::endl;
 	else
 		std::cout << "char: \'" << char(aux) << "'" << std::endl;
-	if (str.length() < max.length() || (strcmp(str.c_str(), max.c_str()) < 1 && str.length() == max.length()))
+	if (str == "+inff" || str == "-inff" || str  == "nanf")
+		std::cout << "int: " << "Non displayable"  << std::endl;
+	else if (str.length() < max.length() || (strcmp(str.c_str(), max.c_str()) < 1 && str.length() == max.length()))
 		std::cout << "int: \'" << std::atoi(str.c_str()) << "'" <<  std::endl;
 	else
 		std::cout << "int: " << "Non displayable"  << std::endl;
-	if (str.find(str, '.') && std::atoi(str.c_str() + str.find(str, '.')))
+	if ((str.find(".") < str.length() - 1 && std::atoi(str.c_str() + str.find(".") + 1)) || str == "+inff" || str == "-inff" || str  == "nanf")
 		std::cout << "float: \'" << std::atof(str.c_str()) << "f'" <<  std::endl;
 	else
 		std::cout << "float: \'" << std::atof(str.c_str()) << ".0f'" <<  std::endl;
-	if (str.find(str, '.') && std::atoi(str.c_str() + str.find(str, '.')))
+	if ((str.find(".") < str.length() - 1 && std::atoi(str.c_str() + str.find(".") + 1)) || str == "+inff" || str == "-inff" || str  == "nanf")
 		std::cout << "double: \'" << std::atof(str.c_str()) << "'" <<  std::endl;
 	else
 		std::cout << "double: \'" << std::atof(str.c_str()) << ".0'" <<  std::endl;
@@ -87,38 +89,56 @@ void	doubles(std::string str)
 		std::cout << "char: " << "Non displayable"  << std::endl;
 	else
 		std::cout << "char: \'" << char(aux) << "'" << std::endl;
-	if (str.length() < max.length() || (strcmp(str.c_str(), max.c_str()) < 1 && str.length() == max.length()))
+	if (str == "+inf" || str == "-inf" || str  == "nan")
+		std::cout << "int: " << "Non displayable"  << std::endl;
+	else if (str.length() < max.length() || (strcmp(str.c_str(), max.c_str()) < 1 && str.length() == max.length()))
 		std::cout << "int: \'" << std::atoi(str.c_str()) << "'" <<  std::endl;
 	else
 		std::cout << "int: " << "Non displayable"  << std::endl;
-	if (str.find(str, '.') && std::atoi(str.c_str() + str.find(str, '.')))
+	if ((str.find(".") < str.length() - 1 && std::atoi(str.c_str() + str.find(".") + 1)) || str == "+inf" || str == "-inf" || str  == "nan")
 		std::cout << "float: \'" << std::atof(str.c_str()) << "f'" <<  std::endl;
 	else
 		std::cout << "float: \'" << std::atof(str.c_str()) << ".0f'" <<  std::endl;
-	if (str.find(str, '.') && std::atoi(str.c_str() + str.find(str, '.')))
+	if ((str.find(".") < str.length() - 1 && std::atoi(str.c_str() + str.find(".") + 1)) || str == "+inf" || str == "-inf" || str  == "nan")
 		std::cout << "double: \'" << std::atof(str.c_str()) << "'" <<  std::endl;
 	else
 		std::cout << "double: \'" << std::atof(str.c_str()) << ".0'" <<  std::endl;
 }
 
 int	checkType(std::string str){
+	std::size_t aux = str.find(".");
+
 	if ((str[0] < '0' || str[0] > '9') && str[0] != '+' && str[0] != '-')
 	{
 		if (str.length() == 1)
 			return 0;
 		else if (str == "nan")
 			return 3;
+		else if (str == "nanf")
+			return 2;
 		else
 			return -1;
+	}
+	for (int i = 0; i < int(str.length()); i++){
+		if (!isdigit(str[i])){
+			if (i == int(str.length()) - 1 && str[i] == 'f')
+				continue;
+			else
+				return -1;
+		}
 	}
 	if (str == "-inf" || str == "+inf")
 		return 3;
 	if (str == "-inff" || str == "+inff")
 		return 2;
+	if (!str[aux + 1] || (str[aux + 1] < '0' || str[aux + 1] > '9'))
+		return -1;
 	if (str.back() == 'f')
 		return 2;
-	if (str.find(str, '.'))
+	if (aux > 0 && aux < str.length() - 1)
 		return 3;
+	if (!std::atoi(str.c_str()))
+			return -1;
 	return 1;
 }
 
