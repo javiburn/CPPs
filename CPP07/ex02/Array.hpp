a9809b2a66c0b5ef21f57d9f43b13e4f19409107
javiburn <jsarabia98@gmail.com>
2023-12-20 18:53:17 +0100

    ex02 ready to be evaluated

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,14 +6,79 @@
 /*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 17:35:33 by jsarabia          #+#    #+#             */
-/*   Updated: 2023/12/20 17:42:20 by jsarabia         ###   ########.fr       */
+/*   Updated: 2023/12/20 18:52:40 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
-//pragma once
-# include
+# pragma once
+# include <iostream>
+# include <iomanip>
+# include <exception>
 
-#endif;
+template<typename T>
+class Array{
+	private:
+		int	len;
+		T*	arr;
+	public:
+		Array(void): len(0){ arr = new T[0]; };
+		Array(unsigned int n): len(n) { arr = new T[n]; };
+		Array(const Array& array){
+			this->arr = new T[array.len];
+			this->len = array.size();
+			for (int i = 0; i < array.size(); i++)
+				this->arr[i] = array.arr[i];
+		};
+		Array& operator=(const Array& array){
+			this->arr = new T[array.len];
+			this->len = array.size();
+			for (int i = 0; i < array.size(); i++)
+				this->arr[i] = array.arr[i];
+		};
+		~Array(void){
+			if (this->len > 0)
+				delete[] arr;
+			return;
+		}
+		T& operator[](int index){
+			try{
+				if (index >= this->size())
+						throw IndexTooHigh();
+				else if (index < 0)
+						throw IndexTooLow();
+			}
+			catch (std::exception& e){
+				std::cerr << e.what() << std::endl;
+			}
+			return this->arr[index];
+		}
+		int	size()const { return this->len; };
+		void	printArray(void){
+			for (int i = 0; i < this->len; i++)
+				std::cout << i << ": \'" << this->arr[i] << "\'" << std::endl;
+		}
+		void	setArrIndex(const T& temp, int index){
+			try{
+				if (index >= this->size())
+					throw IndexTooHigh();
+			}
+			catch (std::exception& e){
+				std:: cerr << e.what() << std::endl;
+			}
+			this->arr[index] = temp;
+		}
+
+		class IndexTooHigh: public std::exception{
+			const char* what() const throw(){ return "The index is too high"; };
+		};
+
+		class IndexTooLow: public std::exception{
+			const char* what() const throw(){ return "The index is too low"; };
+		};
+
+};
+
+#endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,12 +6,77 @@
 /*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 17:34:51 by jsarabia          #+#    #+#             */
-/*   Updated: 2023/12/20 17:35:07 by jsarabia         ###   ########.fr       */
+/*   Updated: 2023/12/20 18:50:43 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
 int main(){
+	Array<std::string> arr(8);
+	Array<std::string> newarr(arr);
+
+	std::cout << arr.size() << std::endl;
+	std::cout << newarr.size() << std::endl;
+	arr.setArrIndex("Hola", 0);
+	arr[6] = "Jamón";
+	arr[8] = "Jamón";
+	arr.printArray();
+	newarr.printArray();
 	return 0;
 }
+
+// #include <iostream>
+// #include "Array.hpp"
+
+// #define MAX_VAL 750
+// int main(int, char**)
+// {
+//     Array<int> numbers(MAX_VAL);
+//     int* mirror = new int[MAX_VAL];
+//     srand(time(NULL));
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         const int value = rand();
+//         numbers[i] = value;
+//         mirror[i] = value;
+//     }
+//     //SCOPE
+//     {
+//         Array<int> tmp = numbers;
+//         Array<int> test(tmp);
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         if (mirror[i] != numbers[i])
+//         {
+//             std::cerr << "didn't save the same value!!" << std::endl;
+//             return 1;
+//         }
+//     }
+//     try
+//     {
+//         numbers[-2] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+//     try
+//     {
+//         numbers[MAX_VAL] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         numbers[i] = rand();
+// 		std::cout << numbers[i] << std::endl;
+//     }
+//     delete [] mirror;//
+//     return 0;
+// }
